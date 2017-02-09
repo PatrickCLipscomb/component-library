a3f9268878f221217db56877b481065ec5b6c9e8
patrick lipscomb <patrick74lipscomb@gmail.com>
2017-02-08 21:00:23 -0800

    add mocha config file and prepare for more tests

--- src/Maps/LeafletMap.test.js
+++ src/Maps/LeafletMap.test.js
@@ -5,12 +5,13 @@ import LeafletMap from './LeafletMap';
 // Getting there, will update soon...
 // TESTING SAYS REFEENCE ERROR: WINDOW IS NOT DEFINED
 
+
+// can use wrapper.setProps() to set/change the props that are put in, and you can test that
+
 describe('LeafletMap', function() {
-  beforeEach(() => {
-    var window = {};
-  })
+
   const wrapper = shallow(<LeafletMap />);
-  console.log(<LeafletMap />);
+  console.log(shallow(<LeafletMap />).debug());
   it('should expect to find a div element', () => {
     expect(wrapper.find('div')).have.length(1);
   })
